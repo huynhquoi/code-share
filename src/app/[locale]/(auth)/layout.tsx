145bd37f550457b88ffc8c8b35747cb8a21e89bf
Huynh Quoi <hvquoi.cv@gmail.com>
2025-10-26 22:22:23 +0700

    fix: update env url for schema prisma

--- src/app/[locale]/(auth)/layout.tsx
+++ src/app/[locale]/(auth)/layout.tsx
@@ -3,9 +3,7 @@ import { redirect } from "next/navigation";
 
 interface AuthLayoutProps {
   children: React.ReactNode;
-  params: {
-    locale: string;
-  };
+  params: Promise<{ locale: string }>;
 }
 
 export default async function AuthLayout({
